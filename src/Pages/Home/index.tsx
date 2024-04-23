de96428b371b144ca5bcf888c6d598b264788c31
henriquerick12 <henriquewood3@gmail.com>
2024-04-23 23:21:39 +0100

    Buscando dados da api

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,33 +1,62 @@
-import { Link } from "react-router-dom";
+import { FormEvent, useEffect, useState } from "react";
 import { Container, Form, Repositories } from "./styled";
+import { Link } from "react-router-dom";
 
-import {FiChevronRight} from 'react-icons/fi'
+import api from "../../services/api";
+
+import { FiChevronRight } from "react-icons/fi";
 import Logo from "../../assets/logo.svg";
 
+type repoType = {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+};
+
 const Home: React.FC = () => {
+  const [repo, setRepo] = useState<repoType[]>([]);
+  const [newrepo, setNewRepo] = useState("");
+
+  async function handleAddRepo(e: FormEvent<HTMLFormElement>): Promise<void> {
+    e.preventDefault();
+
+    const response = await api.get(`repos/${newrepo}`);
+    setRepo([...repo, response.data]);
+    setNewRepo('')
+  }
+
+  useEffect(() => {}, []);
+
   return (
     <Container>
       <img src={Logo} alt="Logo" />
       <h1>Explore repositorios no Gitbub</h1>
 
-      <Form>
-        <input type="text" placeholder="Digite o nome/repositorio ..." />
+      <Form onSubmit={handleAddRepo}>
+        <input
+          value={newrepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          type="text"
+          placeholder="Digite o nome/repositorio ..."
+        />
         <button>Pesquisar</button>
       </Form>
 
       <Repositories>
-        <Link to={"/repositorio"}>
-          <img
-            src="https://avatars.githubusercontent.com/u/33300487?v=4"
-            alt="git"
-          />
-          <div>
-            <strong>Henrique Silva</strong>
-            <small>Descrição</small>
-          </div>
-          
-          <FiChevronRight size={20}/>
-        </Link>
+        {repo &&
+          repo.map((item) => (
+            <Link key={item.full_name} to={"/repository"}>
+              <img src={item.owner.avatar_url} alt={item.owner.login} />
+              <div>
+                <strong>{item.full_name}</strong>
+                <small>{item.description}</small>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
+          ))}
       </Repositories>
     </Container>
   );
